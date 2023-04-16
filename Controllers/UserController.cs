df8fa63e87972b95a2a9f9acf43b872dee55adaf
Tyndare01 <100364496+Tyndare01@users.noreply.github.com>
2023-04-16 18:41:01 +0200

    test commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using BLL.Interfaces;
+﻿using System.Security.Claims;
+using BLL.Interfaces;
 using DAL.Models;
 using DAL.Models.DTO;
 using DAL.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIAuthTest.Controllers;
@@ -23,7 +25,9 @@ public class UserController : ControllerBase
     // Récupère des DTOs, chèque leurs validités (exemple : authentification Ok?)
     // et renvoit au serviceUser qui modele le DTO en un objet final qu'il envoit au Ripository
     // Le ripository renvoit l'objet final et le retransforme en viewmodel et le renvoit au contrôleur 
-    [HttpPost]
+
+    #region Register
+    [HttpPost("register")]
     public ActionResult<UserViewModel> Register(CreateUserDto createDto)
     {
         if (ModelState.IsValid)
@@ -40,6 +44,210 @@ public class UserController : ControllerBase
 
         return BadRequest();
     }
+    #endregion
+    
+    #region Login
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(LoginDTO loginDto)
+    {
+        if (ModelState.IsValid)
+        {
+            string? jwt = _userService.Login(loginDto);
+
+            if (!string.IsNullOrEmpty(jwt)) 
+            {
+                return Ok(jwt);
+            }
+
+        }
+
+        return BadRequest();
+    }
+    
+    #endregion
+    
+    #region ChangePassword
+    [HttpPatch("password/{id})")]
+    [Authorize]
+    public ActionResult<UserViewModel> ChangePassword(int id, ChangePasswordDTO changePasswordDto)
+    {
+
+        if (id.ToString() != User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value)
+        {
+            return Unauthorized();
+        }
+        
+        // seul l'utilisateur peut changer son mot de passe 
+        // (il faut que l'id de l'utilisateur soit le même que celui du token)
+        // (le token est un objet qui contient des claims, dont le claim NameIdentifier qui est l'id de l'utilisateur)
 
 
+        if (ModelState.IsValid)
+        {
+            UserViewModel? userViewModel = _userService.UpdatePassword(id, changePasswordDto);
+
+            if (userViewModel is not null)
+            {
+                return Ok(userViewModel);
+            }
+            
+        }
+
+        return BadRequest();
+
+    }
+    #endregion
+    
+    #region ChangePhoneNumber
+
+    [HttpPatch("phone/{id})")]
+    [Authorize]
+    
+    public ActionResult<UserViewModel> ChangePhoneNumber(int id, ChangePhoneNumberDTO changePhoneNumberDto)
+    {
+
+        if (id.ToString() != User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value)
+        {
+            return Unauthorized();
+        }
+        
+        if (ModelState.IsValid)
+        {
+            UserViewModel? userViewModel = _userService.UpdatePhoneNumber(id, changePhoneNumberDto);
+
+            if (userViewModel is not null)
+            {
+                return Ok(userViewModel);
+            }
+            
+        }
+
+        return BadRequest();
+
+    }
+
+    #endregion
+    
+    #region ChangeDatas
+
+    [HttpPatch("data/{id})")]
+    [Authorize]
+    public ActionResult<UserViewModel> ChangeDatas(int id, ChangeDataDTO changeData)
+    {
+        
+        if (id.ToString() != User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value)
+        {
+            return Unauthorized();
+        }
+        
+        if (ModelState.IsValid)
+        {
+            UserViewModel? userViewModel = _userService.UpdateDatas(id, changeData);
+
+            if (userViewModel is not null)
+            {
+                return Ok(userViewModel);
+            }
+            
+        }
+
+        return BadRequest();
+
+    }
+
+    #endregion
+
+    #region ChangeRole
+    [Authorize(Roles = "Admin")]
+
+    [HttpPatch("role/{id})")]
+    public ActionResult<UserViewModel> ChangeRole(int id, ChangeRoleDTO changeRole)
+    {
+        
+        
+        
+        if (ModelState.IsValid)
+        {
+            UserViewModel? userViewModel = _userService.UpdateRole(id, changeRole);
+
+            if (userViewModel is not null)
+            {
+                return Ok(userViewModel);
+            }
+            
+        }
+
+        return BadRequest();
+
+    }
+
+    #endregion
+
+    #region GetById
+
+    [HttpGet("{id:int}")]
+    [Authorize]
+    public ActionResult<UserViewModel> GetById(int id)
+    {
+        if (ModelState.IsValid)
+        {
+            UserViewModel? user = _userService.GetById(id);
+            return user is not null ? Ok(user) : BadRequest();
+        }
+
+        return BadRequest();
+    }
+
+    #endregion
+    
+    #region GetByEmail
+
+    [HttpGet("{email}")]
+    [Authorize]
+    public ActionResult<UserViewModel> GetByEmail(string email)
+    {
+        if (ModelState.IsValid)
+        {
+            UserViewModel? user = _userService.GetByEmail(email);
+            return user is not null ? Ok(user) : BadRequest();
+        }
+
+        return BadRequest();
+    }
+
+    #endregion
+
+    #region GetAll
+    [HttpGet]
+    [Authorize]
+    public ActionResult<IEnumerable<UserViewModel>> GetAll()
+    {
+        return  Ok(_userService.GetAll());
+    }
+    #endregion
+
+    #region Delete
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+        
+        if (id.ToString() != User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value)
+        {
+            return Unauthorized();
+        }
+        
+        if (ModelState.IsValid)
+        {
+            return _userService.Delete(id) ? Ok() : BadRequest();
+        }
+
+        return BadRequest();
+    }
+
+    #endregion
+    
 }
+
--- Program.cs
+++ Program.cs
@@ -1,23 +1,74 @@
+using System.Runtime.CompilerServices;
+using System.Text;
 using BLL.Interfaces;
 using BLL.Services;
 using DAL.Context;
 using DAL.Interfaces;
 using DAL.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Remarque : la clef secrete est dans l'appsettings.json
+// on utilise toujours la même clef secrete (dans differentes APIs)
+// permet de decoder notre token et verifier sa validité 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration["jwt:issuer"],
+                
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration["jwt:issuer"],
+                
+                ValidateLifetime = true,
+                
+                
+                // clef signée que le token va contenir => determine si le token est valide ou pas
+                // On doit convertir en UTF8 notre tableau de bytes (la clef secrète)
+                
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:key"]))
+                
+
+            };
+        });
 
 // Add services to the container.
 
 builder.Services.AddDbContext<AuthDbContext>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJwtService, JwtService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(options =>
+    {
+        options.AllowAnyOrigin();
+        options.AllowAnyHeader();
+        options.WithMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+        
+    });
+
+
+});
+
 var app = builder.Build();
+
+app.UseCors("GetOnly");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,8 +77,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
